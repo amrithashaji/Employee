014e96ba855354555da9f3a282ada977b53794cd
amritha <amrithalshaji@gmail.com>
2019-10-22 23:13:01 +0530

    Employ committed

--- employee_app/models.py
+++ employee_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
     employee_name = models.CharField(max_length=255)
-    image_as_a_blob = models.ImageField(upload_to = 'static/image', default = 'pic_folder/None/no-img.jpg')
+    image_as_a_blob = models.ImageField(upload_to = 'static/image/', default = 'static/None/no-img.jpg')
     email = models.EmailField(max_length=75)
     password = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
